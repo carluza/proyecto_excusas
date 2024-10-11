e770072db3dac41284d9795d0287f3515a5b0277
carluza <111885660+carluza@users.noreply.github.com>
2024-10-11 23:39:10 +0000

    primer-commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
   console.log("Hello Rigo from the console!");
+
+  const getRandomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  const generateExcuse = () =>
+    `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(
+      what
+    )} ${getRandomElement(when)}`;
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
